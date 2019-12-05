b2cd728c989d9087f8298c58eaf7899afad3f7dd
Nick Palomino <nbpalomino@gmail.com>
2019-12-05 14:11:10 -0500

    Funcionalidad para soportar Horus v2

--- HorusOauthSecurityStrategy.js
+++ HorusOauthSecurityStrategy.js
@@ -1,74 +1,45 @@
-const axios = require('axios');
-var jp = require('jsonpath');
-var HorusRestClient = require('./client/HorusRestClient.js');
+const HorusRestClient = require('./client/HorusRestClient.js');
 
 function HorusOauthSecurityStrategy(expressServer, options) {
+  logger.info(options);
 
   var _this = this;
   var horusRestClient = new HorusRestClient(options.horusBaseUrl);
 
-  // this endpoint will be called by external platforms sending us the code
   expressServer.get(options.express.callbackRoute, function(req, res) {
 
     var authorizationCode = req.query.code;
 
     if (!authorizationCode || (typeof authorizationCode === 'undefined')) {
-      console.log("oauth authorization code is undefined. This is very rare. Is the earth planet alive?");
+      logger.error("oauth authorization code is undefined. This is very rare. Is the earth planet alive?");
       res.redirect(options.express.failureRedirectRoute);
       return;
     }
 
-    //get login redirect url
-    var queryParameters = {
-      idTypeLogin: options.oauth.idTypeLogin,
-      clientId: options.oauth.clientId,
-      clientSecret: options.oauth.clientSecret,
-      code: authorizationCode,
-      username: ''
-    };
+    logger.info("Authorization new user with code: "+authorizationCode);
+    options.horusOptions.authenticate.authorizationCode = authorizationCode;
 
-    console.log("Requesting new token");
-    horusRestClient.getAccessTokenAndSubject(queryParameters, function(getAccessTokenAndSubjectErr, accessTokenAndSubject) {
-      if (getAccessTokenAndSubjectErr) {
-        console.log(getAccessTokenAndSubjectErr);
+    horusRestClient.authenticate(options.horusOptions.authenticate, function(getAuthorizeUrlErr, userConfig) {
+      if (getAuthorizeUrlErr) {
+        logger.error("Error in auth transaction: "+getAuthorizeUrlErr);
         res.redirect(options.express.failureRedirectRoute);
         return;
       }
+      logger.info("Mapping menu from response");
+      mapMenuReferences(userConfig.options, options);
 
-      //get login redirect url
-      var bodyParameters = {
-        accessToken: accessTokenAndSubject.accessToken,
-        typeOptionId: 'ROUTE'
-      };
+      req.session.userLogguedIn = userConfig;
+      req.session.save();
 
-      console.log("Requesting route options");
-      horusRestClient.getOptions(bodyParameters,function(getOptionsErr, webOptions){
-        if (getOptionsErr) {
-          console.log(getOptionsErr);
-          res.redirect(options.express.failureRedirectRoute);
-          return;
-        }
+      if (req.session.originalUrl) {
+        res.redirect(req.session.originalUrl);
+      } else {
+        res.redirect(options.express.defaultSuccessLoginRoute);
+      }
 
-        if(webOptions.length == 0){
-          console.log("warning: Options are empty");
-        }
-
-        var userLogguedInfo = {};
-        userLogguedInfo.accessToken = accessTokenAndSubject.accessToken;
-        userLogguedInfo.subject = accessTokenAndSubject.subject;
-        userLogguedInfo.routes = webOptions;
-
-        req.session.userLogguedIn = userLogguedInfo;
-        req.session.save();
-
-        if (req.session.originalUrl) {
-          res.redirect(req.session.originalUrl);
-        } else {
-          res.redirect(options.express.defaultSuccessLoginRoute);
-        }
-
-      });
+      return;
     });
+
   });
 
   this.ensureAuthenticated = function(req, res, next) {
@@ -81,32 +52,34 @@ function HorusOauthSecurityStrategy(expressServer, options) {
       //User is already logued in
       return next();
     } else {
-      console.log("user not logged in");
+      logger.info("User not logged in");
 
-      //get login redirect url
-      var queryParameters = {
-        idTypeLogin: options.oauth.idTypeLogin,
-        clientId: options.oauth.clientId,
-        clientSecret: options.oauth.clientSecret
-      };
-
-      horusRestClient.getAuthorizeUrl(queryParameters, function(getAuthorizeUrlErr, authorizeUrl) {
+      logger.info(options.horusOptions.authorizeUrl);
+      horusRestClient.getAuthorizeUrl(options.horusOptions.authorizeUrl, function(getAuthorizeUrlErr, authorizeUrl) {
         if (getAuthorizeUrlErr) {
-          console.log(getAuthorizeUrlErr);
+          logger.error(getAuthorizeUrlErr);
           res.redirect(options.express.failureRedirectRoute);
           return;
         }
 
-        console.log("Redirect url: " + authorizeUrl);
+        logger.info("Redirect url: " + authorizeUrl);
         res.redirect(authorizeUrl);
         return;
-
       });
-
     }
-
   }
+}
 
+function mapMenuReferences(menuOptions, appOptions) {
+  menuOptions.forEach(opt => {
+    var matched = opt.value.match(appOptions.regexPattern);
+    if(matched) {
+      var baseUrl = appOptions.dependencies[matched[1]];
+      if(baseUrl) {
+        opt.value = opt.value.replace(matched[0], baseUrl);
+      }
+    }
+  });
 }
 
 module.exports = HorusOauthSecurityStrategy;
--- client/HorusRestClient.js
+++ client/HorusRestClient.js
@@ -3,18 +3,59 @@ var jp = require('jsonpath');
 
 function HorusRestClient(horusBaseUrl) {
 
-  var horusGetAuthorizeUrlEndpoint = horusBaseUrl + '/oauth/security/authorize/url';
-  var horusGetAccesstokenEndpoint = horusBaseUrl + '/oauth/security/token';
-  var horusGetOptionsEndpoint = horusBaseUrl + '/oauth/security/option/access';
+  var horusAuthenticateEndpoint = horusBaseUrl + '/v1/nonspec/oauth2/auth';
+  var horusGetAuthorizeUrlEndpoint = horusBaseUrl + '/v1/nonspec/oauth2/auth/url';
 
+  this.authenticate = function(params, callback) {
+    try {
+      axios({
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json'
+          },
+          url: horusAuthenticateEndpoint,
+          data: params,
+        })
+        .then(function(horusResponse) {
+
+          if (!horusResponse || (typeof horusResponse === 'undefined')) {
+            return callback("Horus " + horusGetOptionsEndpoint + " http response is wrong.", null);
+          }
+
+          if (!horusResponse.data || (typeof horusResponse.data === 'undefined')) {
+            return callback("Horus " + horusGetOptionsEndpoint + " http response body is null, empty or wrong.", null);
+          }
+
+          var status = jp.query(horusResponse.data, '$.status');
+
+          if (status != "200") {
+            return callback("Horus " + horusGetOptionsEndpoint + " json response contains [status] different to 200:" + JSON.stringify(horusResponse.data), null);
+          }
+
+          return callback(null, horusResponse.data.content);
+
+        })
+        .catch(function(err) {
+          logger.error(err.response);
+          return callback("Horus is down or " + horusGetOptionsEndpoint + " does not respond: " + err.message, null);
+        });
+    } catch (globalErr) {
+      logger.error(globalErr.stack);
+      return callback("Error when consuming Horus service " + horusGetOptionsEndpoint + ":" + globalErr.message, null);
+    }
+
+  }
 
   this.getAuthorizeUrl = function(params, callback) {
 
     try {
       axios({
-          method: 'get',
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json'
+          },
           url: horusGetAuthorizeUrlEndpoint,
-          params: params
+          data: params
         })
         .then(function(horusResponse) {
           if (!horusResponse || (typeof horusResponse === 'undefined')) {
@@ -36,128 +77,21 @@ function HorusRestClient(horusBaseUrl) {
           if (!horusResponse.data.content || (typeof horusResponse.data.content === 'undefined')) {
             return callback("Horus " + horusGetAuthorizeUrlEndpoint + " http response content is undefined. Redirect url was expected :" + horusResponse.data.content, null);
           }
-
-          return callback(null, horusResponse.data.content);
+          return callback(null, horusResponse.data.content.url);
 
         })
         .catch(function(err) {
-          console.error(err.stack);
+          logger.error(err.response);
+          logger.error("Error: "+err.response.data.status+", message:"+err.response.data.message);
           return callback("Horus is down or " + horusGetAuthorizeUrlEndpoint + " does not respond: " + err.message, null);
         });
     } catch (globalErr) {
-      console.error(globalErr.stack);
+      logger.error(globalErr.stack);
       return callback("Error when consuming Horus service:" + globalErr.message, null);
     }
 
   }
-
-  this.getAccessTokenAndSubject = function(params, callback) {
-
-    try {
-      axios({
-          method: 'get',
-          url: horusGetAccesstokenEndpoint,
-          params: params
-        })
-        .then(function(horusResponse) {
-
-          if (!horusResponse || (typeof horusResponse === 'undefined')) {
-            return callback("Horus " + horusGetAccesstokenEndpoint + " http response is wrong.", null);
-          }
-
-          if (!horusResponse.data || (typeof horusResponse.data === 'undefined')) {
-            return callback("Horus " + horusGetAccesstokenEndpoint + " http response body is null, empty or wrong.", null);
-          }
-
-          var status = jp.query(horusResponse.data, '$.status');
-
-          if (status != "200") {
-            return callback("Horus " + horusGetAccesstokenEndpoint + " json response contains [status] different to 200:" + JSON.stringify(horusResponse.data), null);
-          }
-
-          //jsonpath return an array instead unique value : https://stackoverflow.com/a/23624178/3957754
-          var accessToken = jp.value(horusResponse.data, '$.content.accessToken');
-
-          if (!accessToken || (typeof accessToken === 'undefined')) {
-            return callback("Horus " + horusGetAccesstokenEndpoint + " json response dos not contain [content.accessToken] or is empty:" + JSON.stringify(horusResponse.data), null);
-          }
-
-          //jsonpath return an array instead unique value : https://stackoverflow.com/a/23624178/3957754
-          var subject = jp.value(horusResponse.data, '$.content.nickName');
-
-          if (!subject || (typeof subject === 'undefined')) {
-            return callback("Horus " + horusGetAccesstokenEndpoint + " json response dos not contain [content.nickName] or is empty:" + JSON.stringify(horusResponse.data), null);
-          }
-
-          return callback(null, {
-            "accessToken": accessToken,
-            "subject": subject
-          });
-
-        })
-        .catch(function(err) {
-          console.error(err.stack);
-          console.log(err.response.data);
-          return callback("Horus is down or " + horusGetAccesstokenEndpoint + " does not respond: " + err.message, null);
-        });
-    } catch (globalErr) {
-      console.error(globalErr.stack);
-      return callback("Error when consuming Horus service:" + globalErr.message, null);
-    }
-
-  }
-
-  this.getOptions = function(params, callback) {
-
-    try {
-      axios({
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json'
-          },
-          url: horusGetOptionsEndpoint,
-          data: {
-            accessToken: params.accessToken,
-            typeOptionId: 'ROUTE',
-          },
-        })
-        .then(function(horusResponse) {
-
-          if (!horusResponse || (typeof horusResponse === 'undefined')) {
-            return callback("Horus " + horusGetOptionsEndpoint + " http response is wrong.", null);
-          }
-
-          if (!horusResponse.data || (typeof horusResponse.data === 'undefined')) {
-            return callback("Horus " + horusGetOptionsEndpoint + " http response body is null, empty or wrong.", null);
-          }
-
-          var status = jp.query(horusResponse.data, '$.status');
-
-          if (status != "200") {
-            return callback("Horus " + horusGetOptionsEndpoint + " json response contains [status] different to 200:" + JSON.stringify(horusResponse.data), null);
-          }
-
-          if (Object.prototype.toString.call(horusResponse.data.content) !== '[object Array]') {
-            return callback("Horus " + horusGetOptionsEndpoint + " json response contains [content] which is not an array:" + JSON.stringify(horusResponse.data), null);
-          }
-
-          return callback(null, horusResponse.data.content);
-
-        })
-        .catch(function(err) {
-          console.error(err.stack);
-          console.log(err.response.data);
-          return callback("Horus is down or " + horusGetOptionsEndpoint + " does not respond: " + err.message, null);
-        });
-    } catch (globalErr) {
-      console.error(globalErr.stack);
-      return callback("Error when consuming Horus service " + horusGetOptionsEndpoint + ":" + globalErr.message, null);
-    }
-
-  }
-
 }
 
 
 module.exports = HorusRestClient
-
